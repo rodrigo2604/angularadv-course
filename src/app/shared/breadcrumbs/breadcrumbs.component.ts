f33913386eb2b881396fff682b7c5080571b51ad
Rodrigo Llanos <rodrigo.llanosordonez@altran.com>
2018-05-21 16:28:52 +0200

    [FEAT] Breadcrumbs con observers y metatags

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivationEnd } from '@angular/router';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
+
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -7,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumbsComponent implements OnInit {
 
-  constructor() { }
+  label: string;
+
+  constructor(
+    private router: Router,
+    public title: Title,
+    public meta: Meta
+  ) { }
 
   ngOnInit() {
+    this.getDataRoute().subscribe(({ titulo }) => {
+      this.label = titulo;
+      this.title.setTitle(titulo);
+
+      const metaTag: MetaDefinition = {
+        name: 'description',
+        content: this.label
+      };
+
+      this.meta.updateTag(metaTag);
+    });
+  }
+
+  getDataRoute() {
+    return this.router.events
+      .filter(evento => evento instanceof ActivationEnd)
+      .filter((evento: ActivationEnd) => evento.snapshot.firstChild === null)
+      .map((evento: ActivationEnd) => evento.snapshot.data);
   }
 
 }
